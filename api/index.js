163b46b9e2d963aeab8b11619fa3af746dcf0dd7
Artos Web <teodor.at.tod@gmail.com>
2022-10-28 22:46:00 +0300

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-import e from 'express';
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log('Connected to DB');
+    } catch (error) {
+        throw error;
+    }
+}
 app.get('/', (req, res) => {
-    res.send('lalalalla')
+    res.send('opppoo')
 });
 
-app.listen(8800, console.log('Server is running on port 8800'))
+app.listen(8800, () => {
+    connect()
+    console.log('Server is running on port 8800')
+})
