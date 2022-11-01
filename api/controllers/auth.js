2279c6ebc57caac52325083152f7fdc162191ce8
Artos Web <teodor.at.tod@gmail.com>
2022-11-01 22:51:36 +0200

    add logi controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js "
 import bcrypt from 'bcrypt';
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
     try {
@@ -14,6 +15,24 @@ export const register = async (req, res, next) => {
 
         await newUser.save();
         res.status(200).send('User has been created');
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) return next(createError(404, "User not found!"));
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password);
+        if (!isPasswordCorrect)
+            return next(createError(404, "Wrong password/username!"));
+
+            const {password, isAdmin, ...otherDetails} = user._doc;
+        res.status(200).json({...otherDetails});
     } catch (err) {
         next(err)
     }
