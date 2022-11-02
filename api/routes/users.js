f8c39b120533d97b9c1e61dc758a461db8d2d60c
Artos Web <teodor.at.tod@gmail.com>
2022-11-02 22:07:59 +0200

    add checkuser util

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js';
+import { verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+    res.send('Hello, user, you are logged in')
+});
+
+router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+    res.send('Hello, user, you are logged in and you can delete your account')
+});
 
 //UPDATE
 router.put('/:id', updateUser);
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { createError } from './error';
+import { createError } from './error.js';
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
@@ -13,5 +13,15 @@ export const verifyToken = (req, res, next) => {
         }
         req.user = user;
         next();
+    })
+}
+
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+                return next(createError(403, "You are not authorized"))
+        }
     })
 }
