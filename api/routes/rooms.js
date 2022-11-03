ecee4879e231a7c23fc4dd51e5c604ee5ce16e20
Artos Web <teodor.at.tod@gmail.com>
2022-11-03 22:20:05 +0200

    add room routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
 
 const router = express.Router();
 
+//CREATE
+router.post('/:hotelId', verifyAdmin, createRoom);
+//UPDATE
+router.put('/:id', verifyAdmin, updateRoom);
+//DELETE
+router.delete('/:id', deleteRoom);
+//GET
+router.get('/:id', getRoom);
+//GET ALL
+router.get('/', getRooms);
 
 export default router;
