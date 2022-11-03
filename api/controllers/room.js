ae1934df357a8eef2d9f2928463583c5eeebe797
Artos Web <teodor.at.tod@gmail.com>
2022-11-03 22:01:25 +0200

    add room controller

---
+++ api/controllers/room.js
@@ -0,0 +1,20 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    const newRoom = new Room(req.body);
+
+    try {
+        const savedRoom = await newRoom.save();
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {$push})
+        } catch (err) {
+            next(err);
+        }
+    } catch (err) {
+        next(err);
+    }
+
+}
